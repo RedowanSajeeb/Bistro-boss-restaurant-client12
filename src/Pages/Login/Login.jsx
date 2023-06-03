04a27460d679dadb4861329630b9b79afb4fc19a
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-04 00:15:29 +0600

    loginpage

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,52 +1,106 @@
 // import React from 'react';
+import { useEffect, useRef, useState } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
 
 const Login = () => {
-    return (
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
+  const useCaptchaRef = useRef(null);
+
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const loginHandeler = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
+
+  const captchaHAndelerChange = () => {
+    const value = useCaptchaRef.current.value;
+    console.log(value);
+    if (validateCaptcha(value)) {
+      // alert("Captcha Matched");
+      setDisabled(false);
+      useCaptchaRef.current.value = " ";
+    } else {
+      setDisabled(true);
+    }
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content   flex-col ">
+        <div className="text-center w-96 max-w-sm  lg:text-left">
+          <h1 className="text-5xl text-center mb-7 font-bold">Login</h1>
+        </div>
+        <form
+          onSubmit={loginHandeler}
+          className="card flex w-full max-w-sm shadow-2xl bg-base-100"
+        >
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                name="email"
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <button href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </button>
+              </label>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <LoadCanvasTemplate />
+              </label>
+              <input
+                ref={useCaptchaRef}
+                type="text"
+                name="captcha"
+                placeholder="Type captcha above"
+                className="input input-bordered"
+              />
+              <button
+                onClick={captchaHAndelerChange}
+                className="btn btn-xs btn-outline mt-3"
+              >
+                Captcha Match
+              </button>
+            </div>
+            <div className="form-control mt-6 bg-[rgba(209, 160, 84, 0.7)]">
+              <button disabled={disabled} className="btn btn-warning  text-white">
+                Login
+              </button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
