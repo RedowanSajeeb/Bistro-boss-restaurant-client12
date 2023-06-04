55470875808dc85733a5a10e28529154de9634f5
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-05 00:43:37 +0600

    error

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,42 +1,24 @@
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import Helmett from "../../Components/Helmet/Helmett";
-import { useContext } from "react";
-import { AuthContext } from "../../Provider/AuthProvider";
+
+// import React from 'react';
 
 const Signup = () => {
-  
-    const {
-      register,
-      handleSubmit,
-      // reset ,
-      formState: { errors },
-    } = useForm();
-    const { usercreateWithEmailAndPassword } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-     const onSubmit = (data) => {
-        console.log(data.email, data.password);
-        usercreateWithEmailAndPassword(data.email, data.password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-            console.log(user);
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-            // ..
-          });
 
-        // reset
-    };
-    //  console.log(watch("example")); 
+   const onSubmit = (data) => {
 
-    return (
+    console.log(data)
+  };
+
+
+  return (
+    <div>
       <div className="hero min-h-screen bg-base-200">
-        <Helmett title={"Sign Up "}></Helmett>;
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Login now!</h1>
@@ -59,8 +41,8 @@ const Signup = () => {
                   className="input input-bordered"
                 />
                 {errors.name && (
-                  <span className="mt-1 text-red-600">
-                    Name field is required*
+                  <span className="text-red-600 mt-1">
+                    Name field is required
                   </span>
                 )}
               </div>
@@ -70,13 +52,13 @@ const Signup = () => {
                 </label>
                 <input
                   {...register("email", { required: true })}
-                  type="email"
+                  type="text"
                   placeholder="email"
                   className="input input-bordered"
                 />
                 {errors.email && (
-                  <span className="mt-1 text-red-600">
-                    Email field is required*
+                  <span className="text-red-600 mt-1">
+                    Email field is required
                   </span>
                 )}
               </div>
@@ -89,26 +71,20 @@ const Signup = () => {
                     required: true,
                     minLength: 6,
                     maxLength: 20,
-                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].)(?=.*[a-z])/,
                   })}
-                  type="password"
+                  type="text"
                   placeholder="password"
                   className="input input-bordered"
                 />
                 {errors.password?.type === "required" && (
-                  <p className="mt-1 text-red-600">
-                    password field is required*{" "}
-                  </p>
+                  <span className="text-red-600 mt-1">
+                    Password field is required
+                  </span>
                 )}
                 {errors.password?.type === "minLength" && (
-                  <p className="mt-1 text-red-600">password must be 6 Length</p>
+                  <p>Must 6 Length password </p>
                 )}
-                {errors.password?.type === "pattern" && (
-                  <p className="mt-1 text-red-600">
-                    password must should be one special case letter , one
-                    uppercase letter And one lowercase ,at least one digit,
-                  </p>
-                )}
+
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
                     Forgot password?
@@ -116,23 +92,14 @@ const Signup = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <input
-                  className="btn btn-primary"
-                  type="submit"
-                  value="Sign Up"
-                />
+                <button className="btn btn-primary">Login</button>
               </div>
-              <p className="text-[#D1A054] text-lg text-center">
-                Already registered?
-                <Link className="font-bold" to={"/login"}>
-                  Go to log in
-                </Link>
-              </p>
             </form>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Signup;
