e2d28fd71f92d98d49093ba237b1c490651b461b
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-05 01:06:17 +0600

    done

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 // import React from 'react';
 
 const Signup = () => {
+  const { usercreateWithEmailAndPassword } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
@@ -13,9 +17,21 @@ const Signup = () => {
    const onSubmit = (data) => {
 
     console.log(data)
+    usercreateWithEmailAndPassword(data.email, data.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode,errorMessage);
+        // ..
+      });
+
   };
-
-
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -71,8 +87,7 @@ const Signup = () => {
                     required: true,
                     minLength: 6,
                     maxLength: 20,
-                    pattern:
-                      /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
                   })}
                   type="text"
                   placeholder="password"
@@ -99,9 +114,19 @@ const Signup = () => {
                   </a>
                 </label>
               </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+              <div className="form-control bg-[rgba(209, 160, 84, 0.7) mt-6">
+                <input
+                  className="btn btn-warning"
+                  type="submit"
+                  value="Sign Up"
+                />
               </div>
+              <p className="text-[#D1A054] text-lg text-center">
+                Already registered?
+                <Link className="font-bold" to={"/login"}>
+                  Go to log in
+                </Link>
+              </p>
             </form>
           </div>
         </div>
