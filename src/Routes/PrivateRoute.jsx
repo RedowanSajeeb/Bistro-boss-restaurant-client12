3f82dd2e0da0b127981e4d1c2202d48b6c4cc981
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-05 06:35:47 +0600

    bug

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -5,10 +5,10 @@ import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({children}) => {
     const location = useLocation();
-    const { user, lodding } = useContext(AuthContext);
-    if (lodding){
-        return <span className="loading loading-bars mx-auto w-full"></span>;
-    }
+    const { user,  } = useContext(AuthContext);
+    // if (lodding){
+    //     return <span className="loading loading-bars mx-auto w-full"></span>;
+    // }
       if (user) {
         return children;
       }
