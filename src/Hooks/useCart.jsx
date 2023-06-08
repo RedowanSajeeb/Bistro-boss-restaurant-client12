5f33e23681765f5fbee0d1de7cdb7a63f19bbcc9
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-08 15:17:05 +0600

    d

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
-// import useAxiosSecure from "./useAxiosSecure";
+
+
 const useCart = () => {
     const {user} = useContext(AuthContext)
         const token = localStorage.getItem("access-Token");
@@ -24,11 +25,12 @@ const useCart = () => {
       //  queryFn: async () => {
       //    const response = await axiosSecure(`/carts?email=${user?.email}`);
 
-        //  if (!response.ok) {
-        //    throw new Error("Network response was not ok");
-        //  }
-        console.log(response.data);
-         return response.data;
+      //    if (!response.ok) {
+      //      throw new Error("Network response was not ok");
+      //    }
+      
+        // console.log(response.data);
+         return response.json();
        },
      });
 
